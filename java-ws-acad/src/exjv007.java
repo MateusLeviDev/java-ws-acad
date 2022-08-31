3152b2cfbcf1da8b3a4cc9d4a8831bae185d1fda
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-08-31 16:41:06 -0300

    Operadores de atribuição.

--- java-ws-acad/src/exjv007.java
+++ java-ws-acad/src/exjv007.java
@@ -11,7 +11,7 @@ public class exjv007 {
 		int minutos = sc.nextInt();
 		double conta = 50.0;
 		if (minutos > 100) {
-			conta = conta + (minutos - 100) * 2.0;
+			conta += (minutos - 100) * 2.0;
 		}
 		
 		System.out.printf("Valor da conta = R$ %.2f%n", conta);
