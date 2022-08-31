d944b23bec0dadc92197df52e811f89dc3960a38
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-08-31 17:06:25 -0300

    Upload.

---
+++ java-ws-acad/src/java_ude_alu/jv_classes02.java
@@ -0,0 +1,25 @@
+package java_ude_alu;
+import java.util.Locale;
+import java.util.Scanner;
+public class jv_classes02 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int numero, hora;
+		double valorHora, salario;
+		
+		numero = sc.nextInt();
+		hora = sc.nextInt();
+		valorHora= sc.nextDouble();
+		
+		salario = hora * valorHora; 
+		
+		System.out.println("Número funcionário: " + numero);
+		System.out.printf("Salário: R$ %.2f%n", salario);
+		
+		sc.close();
+	}
+
+}
