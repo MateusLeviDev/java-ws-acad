0c02d10bf21b1a18a75999aa51cfb0c98ae12ed6
mateusleviDev <mateuslevisouzapereira@gmail.com>
2022-08-31 00:51:16 -0300

    java ex001 - math

--- java-ws-acad/src/exemplojava001.java
+++ java-ws-acad/src/exemplojava001.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+public class exemplojava01 {
 
-public class exemplojava001 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, soma; 
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		soma = a + b;
+		
+		System.out.println("Soma: " + soma);
+		
+		sc.close();
+
+	}
 
 }
