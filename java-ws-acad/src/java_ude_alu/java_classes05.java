9ec89043bc11c8beb5c5eba71fe5d7e6d6dea815
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-01 17:18:23 -0300

    [UPDATE] Trabalhando c/ 'for'

---
+++ java-ws-acad/src/java_ude_alu/java_classes05.java
@@ -0,0 +1,24 @@
+package java_ude_alu;
+
+import java.util.Scanner;
+
+public class java_classes05 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int soma = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+		
+		System.out.println(soma);
+
+			sc.close();
+	}
+
+}
