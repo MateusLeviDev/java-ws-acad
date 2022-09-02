0a8f5f98fae5a95cf375e38b11689cab01f75b98
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-02 11:34:37 -0300

    [ESTUDO] OPERADORES BITWISE

---
+++ java-ws-acad/src/java_ude_alu/java_classes07.java
@@ -0,0 +1,27 @@
+package java_ude_alu;
+
+public class java_classes07 {
+
+	public static void main(String[] args) {
+		
+		int n1 = 89;
+		int n2 = 60;
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+	}
+	
+		/* 
+				Scanner sc = new Scanner(System.in);
+				int mask = 0b100000;
+				int n = sc.nextInt();
+				if ((n & mask) != 0) {
+					System.out.println("6th bit is true!");
+				}
+				else {
+					System.out.println("6th bit is false");
+				}
+				sc.close();
+	 * */
+
+}
