f307ceadc6119ac2d09b9a316d09bdbbcc3ffb1e
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-04 23:38:09 -0300

    [UPDATE] DELEGAÇÃO DE RESPONSABILIDADES

--- java-ws-acad/src/entities/triangle.java
+++ java-ws-acad/src/entities/triangle.java
@@ -5,4 +5,12 @@ public class triangle {
 	public double a;
 	public double b;
 	public double c; 
+	
+	// criando agora um método uma função para calcular a área.
+	
+	public double area() {
+		double p = (a + b + c)/2.0;  // trabalhando com os atributos da classe. 
+		double result = Math.sqrt(p * (p - a)*(p - b)*(p - c));
+		return Math.sqrt(p * (p - a)*(p - b)*(p - c));
+	}
 }
