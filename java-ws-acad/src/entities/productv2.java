549c46a1127fd7f4b0adced1afa7f991b9e3f45a
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-06 15:24:07 -0300

    [UPDATE] ENTITIES PROGRAM 02 C/ CONSTRUTORES

---
+++ java-ws-acad/src/entities/productv2.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class productv2 {
+
+		public String name;
+		public double price; 
+		public int quantity;
+		
+		// geralmente os construtores entram depois dos atributos e antes dos métodos
+		
+		public productv2 (String name, double price, int quantity) {
+			this.name = name;
+			this.price = price;
+			this.quantity = quantity;
+		}
+		
+		
+		public double totalValueInStock() {
+			return price * quantity;
+		}
+		
+		public void addProducts(int quantity) {
+			this.quantity += quantity; // acessar o atributo da classe e não esse parâmetro
+		} 
+		public void removeProducts(int quantity) {
+			this.quantity -= quantity;
+		}
+		
+		public String toString() {
+			return name
+				+ ", $ "
+				+ String.format("%.2f", price)
+				+ ", "
+				+ quantity
+				+ " units, Total: $"
+				+ String.format("%.2f", totalValueInStock());
+		}
+}
