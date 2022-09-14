d716d89a01c1c50c75bf3b10ddca003f38edee58
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-14 19:12:40 -0300

    [CLASSES] Laço For each

---
+++ java-ws-acad/src/java_ude_alu/java_classes12.java
@@ -0,0 +1,19 @@
+package java_ude_alu;
+
+public class java_classes12 {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String[] {"maria", "Bob", "Alex"};
+		
+		for (String obj : vect) {
+			System.out.println(obj); //primeiramente: o tipo dos elementos do vetor [String]
+										//em seguida um apelido para cada elemento do vetor [obj] podendo ser qualquer um
+										//depois a coleção: [vect] 
+										//for percorre todos os elementos, tendo o mesmo efeito de fazer o for normal
+			
+		}
+
+	}
+
+}
