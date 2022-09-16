b4a5bd2ab6ded98647cb833e441d09b313955cb5
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2022-09-16 00:05:30 -0300

    [CLASSES] Matrizes

---
+++ java-ws-acad/src/java_ude_alu/java_classes14.java
@@ -0,0 +1,39 @@
+package java_ude_alu;
+
+import java.util.Scanner;
+
+public class java_classes14 {
+
+	public static void main(String[] args) {
+		
+		// Matrizes
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [][] mat = new int [n][n];
+		
+		for (int i=0;i<n;i++) {
+			for (int j=0; j<n;j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Main diagonal: ");
+		for (int i=0;i<n;i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+		int cont = 0;
+		for (int i=0;i<n;i++)  {
+			for (int j=0;j<n;j++) {
+				if (mat[i][j] < 0) {
+					cont++;
+				}
+			}
+		}
+		System.out.println("Negative numbers: " + cont);
+		
+		sc.close();
+	}
+
+}
